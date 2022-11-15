e16cf6d1426a210f001d0d3dcfe9055f59431359
patriciarchy <aidim@uw.edu>
2022-11-14 20:38:39 -0800

    fixed chart 3

--- source/chart_3.R
+++ source/chart_3.R
@@ -13,15 +13,15 @@ aq <-  raw_data %>% select(bldg, room, location, TOD, period)
 aq <- mutate(aq, place = paste0(bldg,  ', ', room))
 
 occupied_count <- aq %>% group_by(place) %>% filter(period == "Forest fire") %>% filter(TOD == "occupied") %>% count(place)
-# occupied_count <- mutate(occupied_count, percentage = n / sum(occupied_count$n))
 
-chart <- pie(occupied_count$n, labels = occupied_count$place, main="Pie Chart for room occupancy during forest fire", col=rainbow(25), cex = 0.4)
-chart 
+data <- data.frame(Rooms = occupied_count$place, Occupancy = occupied_count$n)
 
+chart <- ggplot(data, aes(x = "", y = Occupancy, fill = Rooms)) +
+  geom_col(color = "black") +
+  geom_text(aes(label = occupied_count$n), size = 2,
+            position = position_stack(vjust = 0.5)) +
+  coord_polar(theta = "y")
 
-#This pie chart is used to answer our third research question regarding which rooms 
-#people feel safe to go into during forest fire as air quality decreases. 
-#It shows the occupancy rate of each room during forest fires. 
-#We noticed that a few rooms have the exact same occupancy rate, it's potentially due to mandatory tasks and responsibilities
-#that have to be fulfilled in that space. 
-#Viewers can clearly distinguish people's gravitation towards each room in this chart as labels are clear and readible. 
+display_chart_3 <- function() {
+  chart
+}
